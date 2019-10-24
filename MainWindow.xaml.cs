9051d75f93c510dc91ae0d829a88c77ed8676b9a
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-24 14:04:47 +0200

    arriver a ecrire sur application mais sans model MVVM

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using ActiveMq_Utils;
+using MvvMTransport;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +23,10 @@ namespace AppliTransportSNCF
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+       
     {
+        public string recup { set; get; }
+        Model model = new Model();
         private string user = ConfigurationManager.AppSettings["ActiveMQ_user"];
         private string pwd = ConfigurationManager.AppSettings["ActiveMQ_pwd"];
         private string host = ConfigurationManager.AppSettings["ActiveMQ_host"];
@@ -33,12 +38,34 @@ namespace AppliTransportSNCF
             InitializeComponent();
             listner = new ActivMQListner(user, pwd, host, port, topic); // lien connexion
             listner.eventMsg += Listner_eventMsg; //abooné a evenement substriber
-            listner.start(); //lancer l'évenement
+                                             // listner.start(); //lancer l'évenement
+            Thread th = new Thread(ListenenMsg);
+          
+            th.Start();
+       
+
         }
+
 
         private void Listner_eventMsg(ActivMQListner l, Apache.NMS.ITextMessage msg)
         {
             Console.WriteLine(msg.Text); // je vais afficher dans la console
+            recup = msg.Text;
+
+            // cette manière n'est pas avec le MVVM !!!!!
+           /* Dispatcher.Invoke(() =>
+            {
+                lab_VilleActuelle.Content = recup;
+
+
+            });*/
+           
+
+        }
+
+        public void ListenenMsg()
+        {
+            listner.start();
         }
     }
 }
