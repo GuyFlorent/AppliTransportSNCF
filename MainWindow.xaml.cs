7bbd5cbb6357489de1d3443431c4582e1631fd82
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-24 11:51:40 +0200

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppliTransportSNCF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string user = ConfigurationManager.AppSettings["ActiveMQ_user"];
+        private string pwd = ConfigurationManager.AppSettings["ActiveMQ_pwd"];
+        private string host = ConfigurationManager.AppSettings["ActiveMQ_host"];
+        private string port = ConfigurationManager.AppSettings["ActiveMQ_port"];
+        private string topic = ConfigurationManager.AppSettings["ActiveMQ_topic"];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
